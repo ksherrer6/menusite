54cd584a986cf031befd405652b506ad69413b7a
ksherrer6 <76628416+ksherrer6@users.noreply.github.com>
2021-01-17 07:58:38 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+TODO:
+make a login slider window thing
+make login work
+make a "favorites" or possibly just a star system for most preffered
+generate random clickable menus
+decide if you want a new page for each menu or just a zoom/focus type property on click
